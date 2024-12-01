8712966fc32dc4fa1ccaa91c055f391e7caa3ff9
Danuka koshitha <danukakoshitha2004@gmail.com>
2024-12-01 16:01:11 +0530

    new commit

--- app.js
+++ app.js
@@ -1,87 +1,41 @@
-let label = document.getElementById("category").value;
-let amount = Number(document.getElementById("amount").value);
-
-const ctx1 = document.getElementById("pieChart");
-
-const labelArrayX = [
-  "Groceries",
-  "Utilities",
-  "Entertainment",
-  "Rent",
-  "Miscellaneous",
-];
-const labelArrayY = [200, 150, 100, 800, 50];
-
-document.getElementById("btn").addEventListener("click", addItem);
-document.getElementById("btnUndo").addEventListener("click", undoItem);
-
-function undoItem() {
-  labelArrayX.pop(label);
-  labelArrayY.pop(amount);
-  console.log(labelArrayX);
-  console.log(labelArrayY);
-}
-
-function addItem() {
-  labelArrayX.push(label);
-  labelArrayY.push(amount);
-  console.log(labelArrayX);
-  console.log(labelArrayY);
-}
+const ctx1 = document.getElementById('pieChart');
 
 new Chart(ctx1, {
-  type: "pie",
+  type: 'pie',
   data: {
-    labels: labelArrayX,
-    datasets: [
-      {
-        label: "5 of Votes",
-        data: labelArrayY,
-        borderWidth: 1,
-      },
-    ],
+    labels: ['Groceries','Bils','Helth','Transport','Shopping','Other'],
+    datasets: [{
+      label: 'LKR',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
   },
   options: {
     scales: {
       y: {
-        beginAtZero: true,
-      },
-    },
-  },
+        beginAtZero: true
+      }
+    }
+  }
 });
 
-const ctx2 = document.getElementById("lineChart");
+const ctx2 = document.getElementById('barChart');
 
 new Chart(ctx2, {
-  type: "line",
+  type: 'bar',
   data: {
-    labels: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Agu",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-    datasets: [
-      {
-        label: "Monthly Spending",
-        data: [500, 600, 550, 700, 800, 650, 720, 750, 690, 780, 840, 950],
-        borderWidth: 1,
-      },
-    ],
+    labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun'],
+    datasets: [{
+      label: 'Monthly Spend Rate(%)',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
   },
   options: {
     scales: {
       y: {
-        beginAtZero: true,
-      },
-    },
-  },
+        beginAtZero: true
+      }
+    }
+  }
 });
